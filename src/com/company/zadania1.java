89da27fe8171435f62d87cb112426ce55b0cba60
das1292 <jakub.klos.10@wp.pl>
2021-10-11 15:19:08 +0200

    Initial commit

---
+++ src/com/company/zadania1.java
@@ -0,0 +1,69 @@
+package com.company;
+
+public class zadania1 {
+
+    public static void main(String[] args) {
+//        zad1
+//        System.out.println(31+29+31);
+//        zad2
+//        int suma=0;
+//        for (int i=0;i<=10;i++)
+//        {
+//            suma+=i;
+//        }
+//        System.out.println(suma);
+//        zad3
+//        int ilocz=1;
+//        for (int i=1;i<=10;i++)
+//        {
+//            ilocz*=i;
+//        }
+//        System.out.println(ilocz);
+//        zad4
+//        float saldoKonta=1000;
+//        for(int i=1;i<=3;i++)
+//        {
+//            if(i>1 && i<=3)
+//            {
+//                saldoKonta+=saldoKonta*0.06;
+//            }
+//            System.out.println( i + " rok = "+saldoKonta);
+//        }
+//        zad5
+//        System.out.println("______\n|JAVA|\n______");
+//        zad6
+//        System.out.println("   /////    \n  +\"\"\"\"\"+  \n (| o o |)\n   | ^ |   \n  | `-' |  \n  +-----+  \n");
+//        zad7
+//        System.out.println("   ***\n     *                   *    *                   *\n     *                   *   *                    *\n     *        ****       *  *                     *\n     *            *      * *         *    *       *\n     *        *****      *  *        *    *       * *****\n     *      *     *      *   *       *    *       *      *\n *   *      *    **      *    *      *    *       *      *\n  ***        ***  *      *     *      ****         ******\n");
+//        zad8
+//        System.out.println("   +    \n  + +   \n +   +  \n+-----+\n| .-. |\n| | | |\n+-+-+-+\n");
+//        zad9
+//        System.out.println("/| _ /| \n( ' ' )       -----\n(  -  )      /Hello\\\n | | |       <Junior|\n(__|__)      \\Coder!/\n              -----");
+//        zad10
+//        System.out.println("1.Fast and Furious\n2.Deadpool\n3.Matrix");
+//        zad11
+//        System.out.println("Dłoń\n\n" +
+//                "Dwadzieścia siedem kości,\n" +
+//                "trzydzieści pięć mięśni,\n" +
+//                "około dwóch tysięcy komórek nerwowych\n" +
+//                "w każdej opuszce naszych pięciu palców.\n" +
+//                "To zupełnie wystarczy,\n" +
+//                "żeby napisać „Mein Kampf”\n" +
+//                "albo „Chatkę Puchatka”.");
+//        zad12
+        System.out.println("* * * * * * =========================================================");
+        System.out.println(" * * * * *  =========================================================");
+        System.out.println("* * * * * * =========================================================");
+        System.out.println(" * * * * *  =========================================================");
+        System.out.println("* * * * * * =========================================================");
+        System.out.println(" * * * * *  =========================================================");
+        System.out.println(" * * * * *  =========================================================");
+        System.out.println("* * * * * * =========================================================");
+        System.out.println("=====================================================================");
+        System.out.println("=====================================================================");
+        System.out.println("=====================================================================");
+        System.out.println("=====================================================================");
+        System.out.println("=====================================================================");
+        System.out.println("=====================================================================");
+    }
+}
