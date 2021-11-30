8a2c34022032f5eb380b7ce7c7e7ddc4bd2ac6f6
das1292 <jakub.klos.10@wp.pl>
2021-11-30 20:40:31 +0100

    Zadania 7

---
+++ src/pl/imiajd/klos/Nauczyciel.java
@@ -0,0 +1,17 @@
+package pl.imiajd.klos;
+
+public class Nauczyciel extends Osoba{
+    private int pensja;
+
+    public Nauczyciel(String nazwisko, int rokUrodzenia, int pensja){
+        super(nazwisko, rokUrodzenia);
+        this.pensja = pensja;
+    }
+
+    public int getPensja(){
+        return this.pensja;
+    }
+    public String toString(){
+        return String.valueOf(this.rokUrodzenia) + "\n" + this.nazwisko + "\n" + this.pensja;
+    }
+}
