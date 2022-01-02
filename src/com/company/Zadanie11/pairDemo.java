ca0bf565a2c7db730e5c49b1a0e2e9e07db73856
das1292 <jakub.klos.10@wp.pl>
2022-01-02 15:41:11 +0100

    Zadania 11

---
+++ src/com/company/Zadanie11/pairDemo.java
@@ -0,0 +1,4 @@
+package com.company.Zadanie11;
+
+public class pairDemo {
+}
---
+++ src/com/company/Zadanie11/pairUtil.java
@@ -0,0 +1,7 @@
+package com.company.Zadanie11;
+
+class PairUtil {
+    public static Pair swap(Pair p){
+        return new Pair(p.getFirst(), p.getSecond());
+    }
+}
